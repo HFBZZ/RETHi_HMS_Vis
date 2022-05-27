06763b14537226148f839cbbe169d70f3b517fb2
Corey Tolbert <ctolbert555@gmail.com>
2022-05-27 15:04:20 -0400

    added comments and cleaned up the dictionary plugin

--- zixu/dictionary-plugin.js
+++ zixu/dictionary-plugin.js
@@ -1,3 +1,6 @@
+/* 
+ * Returns a Promise to retreive the dictionary of telemetry from the dictionary.json file.
+ */
 function getDictionary() {
     return http.get('http://localhost:8080/zixu/dictionary.json')
         .then(function (result) {
@@ -5,14 +8,35 @@ function getDictionary() {
         });
 }
 
+/*
+ * Returns a Promise to generate a heirarchy of folders and files from the dictionary file.
+ * 
+ * Example Hierarchy
+ * { 
+ *    mainFolder1:{ 
+ *        subFolder1: {
+ *            innerFile1: {}
+ *        }, 
+ *        subFolder2: {
+ *            innerFile2: {}, 
+ *            innerFile3: {}
+ *        }, 
+ *        subFile1: {}
+ *    }, 
+ *    mainFile1: {}
+ * } 
+ */
 function getSubsystemHierarchy() {
     return getDictionary().then(function (dictionary) {
         console.log(dictionary);
         var subsystem = {};
+        // Add each telemetry object to the heirarchy
         dictionary.measurements.forEach(telem => {
             var folders = telem.name.split('/');
+            // Iterate down the folders of the current file path
             var currfolder = subsystem;
             for(var i = 0; i < folders.length; i++) {
+                // Create an empty subfolder/file if it doesn't already exist
                 if (currfolder[folders[i]] == null) {
                     currfolder[folders[i]] = {};
                 }
@@ -25,9 +49,14 @@ function getSubsystemHierarchy() {
     });
 }
 
+/*
+ * Specifies the objects, and whether to classify them as folders or telemetry points. 
+ */
 var objectProvider = {
     get: function (identifier) {
+        // Returns a Promise
         return getDictionary().then(function (dictionary) {
+            // Specifies the habitat folder in ROOT
             if (identifier.key === 'habitat') {
                 return {
                     identifier: identifier,
@@ -36,8 +65,10 @@ var objectProvider = {
                     location: 'ROOT'
                 };
             } 
+            // Specifies all subfolders (not in ROOT)
             else if (isNaN(identifier.key)) {
                 console.log("FOUND SUBFOLDER: " + identifier.key);
+                // Makes the display name the most local portion of the pathname
                 var pathname = identifier.key.split('/');
                 return {
                     identifier: identifier,
@@ -46,8 +77,10 @@ var objectProvider = {
                     location: 'habitat.taxonomy'
                 };
             }
+            // Specifies all telemetry
             else {
                 var measurement = dictionary.measurements.find(entry => entry.key === identifier.key);
+                // Makes the display name the most local portion of the pathname
                 var pathname = measurement.name.split('/');
                 return {
                     identifier: identifier,
@@ -63,16 +96,24 @@ var objectProvider = {
     }
 };
 
+/*
+ * Specifies which objects to place directly under the habitat folder in ROOT.
+ */
 var rootCompositionProvider = {
+    // Applies to only the habitat folder in ROOT as the only parent folder
     appliesTo: function (domainObject) {
         return domainObject.identifier.key === 'habitat' &&
                domainObject.type === 'folder';
     },
+    // Specifies all folders and telemetry points to put inside the habitat folder
     load: function (domainObject) {
         return getSubsystemHierarchy().then(function (hierarchy) {
             return getDictionary().then(function (dictionary) {
+                // Add all top-level files and folders
                 return Object.keys(hierarchy).map(function (subfolder) {
+                    // Sets the key as the full path up to the subfolder (assuming it is a folder and not a file)
                     var thisKey = subfolder;
+                    // If the object has no subfolders in the hierarchy, then it is a file, so we should edit the key accordingly
                     if (Object.keys(hierarchy[subfolder]).length == 0) {
                         thisKey = dictionary.measurements.find(element => element.name == subfolder).key;
                     }
@@ -86,23 +127,31 @@ var rootCompositionProvider = {
     }
 };
 
+/*
+ * Specifies which objects to place directly under each folder.
+ */
 var folderCompositionProvider = {
+    // Applies to any folder besides the habitat folder in ROOT
     appliesTo: function (domainObject) {
         return domainObject.identifier.key != 'habitat' &&
                domainObject.identifier.namespace === 'habitat.taxonomy' &&
                domainObject.type === 'folder';
     },
+    // Specifies all folders and telemetry points to put inside the each non-habitat folder
     load: function (domainObject) {
         return getSubsystemHierarchy().then(function (hierarchy) {
             return getDictionary().then(function (dictionary) {
                 var parentFolders = domainObject.identifier.key.split('/');
+                // Navigate down to the the current folder in the hierarchy
                 var thisFolder = hierarchy;
                 for(var i = 0; i < parentFolders.length; i++) {
                     thisFolder = thisFolder[parentFolders[i]]
                 }
-                console.log(thisFolder);
+                // Add all files and folders that are this folder's children in the hierarchy
                 return Object.keys(thisFolder).map(function (subfolder) {
+                    // Sets the key as the full path up to the subfolder (assuming it is a folder and not a file)
                     var thisKey = domainObject.identifier.key + '/' + subfolder;
+                    // If the object has no subfolders in the hierarchy, then it is a file, so we should edit the key accordingly
                     if (Object.keys(thisFolder[subfolder]).length == 0) {
                         thisKey = dictionary.measurements.find(element => element.name == domainObject.identifier.key + '/' + subfolder).key;
                     }
@@ -116,24 +165,25 @@ var folderCompositionProvider = {
     }
 }
 
+/*
+ * Creates the domainObjects and composes them hierarchically
+ */
 var DictionaryPlugin = function (openmct) {
     return function install(openmct) {
+        // Creates the habitat folder in the root directory
         openmct.objects.addRoot({
             namespace: 'habitat.taxonomy',
             key: 'habitat'
         });
 
-        getSubsystemHierarchy().then(function (hier) {
-            console.log("GETTING HIERARCHY");
-        });
-
+        // Specifies the objects
         openmct.objects.addProvider('habitat.taxonomy', objectProvider);
 
+        // Specifies the composition of the objects in the file tree
         openmct.composition.addProvider(rootCompositionProvider);
         openmct.composition.addProvider(folderCompositionProvider);
 
-        console.log("INSTALLING");
-
+        // Creates a custom habitat telemetry point
         openmct.types.addType('habitat.telemetry', {
             name: 'Habitat Telemetry Point',
             description: 'Telemetry point for some habitat object.',
