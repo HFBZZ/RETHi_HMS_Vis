184fd5b3f0e01ce3042e3057859894e79a5015e0
Corey Tolbert <ctolbert555@gmail.com>
2022-06-08 13:47:50 -0400

    updated message sender

--- src/plugins/_MESSAGE_SENDER/MessageSender.js
+++ src/plugins/_MESSAGE_SENDER/MessageSender.js
@@ -1,34 +1,25 @@
-// var dgram = require('dgram');
 
-// var server = dgram.createSocket('udp4');
-
-// server.on('error', function(err) {
-//     console.log(err);
-//     server.close();
-// });
-
-// var socket = new WebSocket("ws://localhost:8889/ws");
+const data_id_dict = {
+    "Thermal Set Point": "7777" // TEMPORARY DATA ID
+};
 
 export default function SendMessage(msg, field_to_modify) {
     console.log("Function wants to send message:", msg, "to update", field_to_modify);
 
-    // socket.send(field_to_modify + ":" + msg);
-
-    // var dgram = require('dgram');
-
-    // var server = dgram.createSocket('udp4');
-
-    // server.on('error', function(err) {
-    //     console.log(err);
-    //     server.close();
-    // });
-    
-    // server.send(msg, port, 'localhost', function (err) {
-    //     if (err) {
-    //         console.log("ERROR SENDING MESSAGE");
-    //     }
-    //     else {
-    //         console.log("Message: '", msg, "' sent to port", port);
-    //     }
-    // });
+    var webaddress = 'ws://localhost:8888/ws/' + data_id_dict[field_to_modify];
+    console.log(webaddress);
+    const ws = new WebSocket(webaddress);
+    ws.onopen = function (event) {
+        ws.send(JSON.stringify({
+            data_type: 1,
+            data: msg,
+        }))
+        console.log("sent")
+    }
+    ws.onmessage = function (event) {
+        console.log(event.data)
+    }
+    ws.onclose = function (event) {
+        console.log(event)
+    }
 }
